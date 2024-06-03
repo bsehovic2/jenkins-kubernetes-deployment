e134181d984306816adeedf83de88017118f8532
Belma Sehovic <bsehovic2@etf.unsa.ba>
2024-06-03 12:04:54 +0200

    tttest

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-      	  Ovo je diploggmski rad Belme Šehović.
+      	  Ovo je diplomski rad Belme Šehović.
         </p>
         <a
           className="App-link"
