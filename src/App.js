2fffcfe9df84d844415ec8ed08da1b28fcfb163d
Belma Sehovic <bsehovic2@etf.unsa.ba>
2024-06-03 09:56:59 +0200

    Izmijenjen tekst - obrisan originalni

--- src/App.js
+++ src/App.js
@@ -7,8 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
- 	  Ovo je diplomski rad Belme Šehović.
+      	  Ovo je diplomski rad Belme Šehović.
         </p>
         <a
           className="App-link"
